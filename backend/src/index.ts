e5445f21384324c11ec4d9dda707bdf9db66f9d2
Welcome ! <usmanubaid9@gmail.com>
2024-01-09 23:12:14 +0100

    feat: db connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import "dotenv/config";
+import mysql from "mysql";
+import { connection } from "./config/db";
 
 const app = express();
 
 const port = process.env.PORT;
+
+connection.connect((error) => {
+  if (error) {
+    console.log("Error connecting to database", error.message);
+  } else {
+    console.log("\x1b[36m%s\x1b[0m", "Connected to Database");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
--- backend/src/types/env.d.ts
+++ backend/src/types/env.d.ts
@@ -4,6 +4,10 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PORT: string;
+      DB_HOST: string;
+      DB_USER: string;
+      DB_PASSWORD: string;
+      DB_DATABASE: string;
     }
   }
 }
