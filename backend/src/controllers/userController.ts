43bb3b92be190a9be06d43dfb88e8ea47b113a50
Welcome ! <usmanubaid9@gmail.com>
2024-01-10 22:52:21 +0100

    feat: query to check username in db

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from "express";
-import { checkExistingEmail } from "../utils/queries";
+import { checkExistingUsername, checkExistingEmail } from "../utils/queries";
 
 const userController = {
   registerUser: async (req: Request, res: Response) => {
     const { username, email, password } = req.body;
-    const checkEmail = await checkExistingEmail(email);
-    if (checkEmail.length > 0) {
+    const dbEmail = await checkExistingEmail(email);
+    if (dbEmail.length > 0) {
       return res.status(400).json({ message: "This email is already in use" });
     }
+    const dbUsername = await checkExistingUsername(username);
+    if (dbUsername.length > 0) {
+      return res.status(400).json({ message: "This username is taken" });
+    }
+
+    res.json({ message: "success" });
   },
 
   loginUser: async (req: Request, res: Response) => {
--- backend/src/utils/queries.ts
+++ backend/src/utils/queries.ts
@@ -2,11 +2,28 @@ import { db } from "../config/db";
 
 export const checkExistingEmail: (
   email: string
-) => Promise<{ email: string }[]> = (email: string) => {
+) => Promise<{ email: string }[]> = (email) => {
   return new Promise((resolve, reject) => {
     db.query(
       `SELECT email FROM users WHERE email = ?`,
       [email],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(results);
+      }
+    );
+  });
+};
+
+export const checkExistingUsername: (
+  username: string
+) => Promise<{ username: string }[]> = (username) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `SELECT username FROM users WHERE email = ?`,
+      [username],
       (error, results) => {
         if (error) {
           reject(error);
