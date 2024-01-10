fb5b9a7ab8aafd7acc7578a6fe1927b5a6065778
Welcome ! <usmanubaid9@gmail.com>
2024-01-10 22:58:37 +0100

    fix: handle error with trycatch

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,19 +1,27 @@
-import { Request, Response } from "express";
+import { Request, Response, ErrorRequestHandler } from "express";
 import { checkExistingUsername, checkExistingEmail } from "../utils/queries";
 
 const userController = {
   registerUser: async (req: Request, res: Response) => {
     const { username, email, password } = req.body;
-    const dbEmail = await checkExistingEmail(email);
-    if (dbEmail.length > 0) {
-      return res.status(400).json({ message: "This email is already in use" });
-    }
-    const dbUsername = await checkExistingUsername(username);
-    if (dbUsername.length > 0) {
-      return res.status(400).json({ message: "This username is taken" });
-    }
 
-    res.json({ message: "success" });
+    try {
+      const dbEmail = await checkExistingEmail(email);
+      if (dbEmail.length > 0) {
+        return res
+          .status(400)
+          .json({ message: "This email is already in use" });
+      }
+      const dbUsername = await checkExistingUsername(username);
+      if (dbUsername.length > 0) {
+        return res.status(400).json({ message: "This username is taken" });
+      }
+
+      res.json({ message: "success" });
+    } catch (error) {
+      console.log(error);
+      return;
+    }
   },
 
   loginUser: async (req: Request, res: Response) => {
