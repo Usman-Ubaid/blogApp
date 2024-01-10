556bf620b44399bfb1d2775cc11b5e70c609f948
Welcome ! <usmanubaid9@gmail.com>
2024-01-10 14:12:24 +0100

    feat: add login user route

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -3,7 +3,11 @@ import { Request, Response } from "express";
 const userController = {
   registerUser: async (req: Request, res: Response) => {
     const { username, email, password } = req.body;
-    res.status(200).json({ username, email, password });
+    return res.status(200).json({ username, email, password });
+  },
+  loginUser: async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    return res.status(200).json({ email, password });
   },
 };
 
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -4,5 +4,6 @@ import userController from "../controllers/userController";
 const router = Router();
 
 router.post("/register", userController.registerUser);
+router.post("/login", userController.loginUser);
 
 export default router;
