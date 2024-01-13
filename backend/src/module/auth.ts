c9e9fedc8cc9d744ece43ad92ab94af602d2fd6a
Welcome ! <usmanubaid9@gmail.com>
2024-01-13 18:11:48 +0100

    feat: bcrypt password comparison function

--- backend/src/module/auth.ts
+++ backend/src/module/auth.ts
@@ -7,6 +7,10 @@ export const hashPassword = (password: string) => {
   return bcrypt.hash(password, saltRounds);
 };
 
+export const comparePassword = (password: string, dbpassword: string) => {
+  return bcrypt.compare(password, dbpassword);
+};
+
 export const generateJWT = (id: number) => {
   if (process.env.JWT_SECRET) {
     return jwt.sign({ id }, process.env.JWT_SECRET);
