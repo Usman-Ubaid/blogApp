77cba1e0fae7bead718aa4bc8cadf2a9e6a795ed
Welcome ! <usmanubaid9@gmail.com>
2024-01-16 22:26:43 +0100

    feat: use useForm custom hook

---
+++ frontend/src/hooks/useForm.ts
@@ -0,0 +1,16 @@
+import { ChangeEvent, useState } from "react";
+
+export const useForm = <T>(initialState: T) => {
+  const [formData, setFormData] = useState<T>(initialState);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+  return { formData, handleInputChange, setFormData };
+};
---
+++ frontend/src/types/form.ts
@@ -0,0 +1,10 @@
+export type RegisterFormData = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export type LoginFormData = {
+  email: string;
+  password: string;
+};
