6b3efb7809e42409215b142f832bd7dbffce98eb
Welcome ! <usmanubaid9@gmail.com>
2024-01-16 22:27:13 +0100

    refactor Login and Register pages

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,17 +1,43 @@
 import Layout from "../components/common/Layout";
-import Form from "../components/form/Form";
+import Input from "../components/form/Input";
+import { useForm } from "../hooks/useForm";
+import { LoginFormData } from "../types/form";
 
 const Login = () => {
-  const loginFields = [
-    { name: "email", placeholder: "Email" },
-    { name: "password", placeholder: "Password" },
-  ];
+  const { formData, handleInputChange, setFormData } = useForm<LoginFormData>({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("olamba");
+  };
+
   return (
     <Layout>
       <div className="form-container">
         <div className="form-wrapper">
           <h2>Login</h2>
-          <Form fields={loginFields} buttonText="Login" />
+          <form onSubmit={handleSubmit}>
+            <Input
+              name="email"
+              placeholder="Email"
+              id="email"
+              onChange={handleInputChange}
+              value={formData.email}
+            />
+            <Input
+              name="password"
+              placeholder="Password"
+              id="password"
+              onChange={handleInputChange}
+              value={formData.password}
+            />
+            <button type="submit" className="btn">
+              Login
+            </button>
+          </form>
         </div>
       </div>
     </Layout>
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,19 +1,47 @@
 import Layout from "../components/common/Layout";
-import Form from "../components/form/Form";
+import { RegisterFormData } from "../types/form";
+import { useForm } from "../hooks/useForm";
+import Input from "../components/form/Input";
 
 const Register = () => {
-  const registerFields = [
-    { name: "username", placeholder: "Username" },
-    { name: "email", placeholder: "Email" },
-    { name: "password", placeholder: "Password" },
-  ];
+  const { formData, handleInputChange, setFormData } =
+    useForm<RegisterFormData>({ username: "", email: "", password: "" });
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("olamba");
+  };
   return (
     <Layout>
       <div className="form-container">
         <div className="form-wrapper">
           <h2>Register</h2>
-          <Form fields={registerFields} buttonText="Register" />
+          <form onSubmit={handleSubmit}>
+            <Input
+              name="username"
+              placeholder="Username"
+              id="username"
+              onChange={handleInputChange}
+              value={formData.username}
+            />
+            <Input
+              name="email"
+              placeholder="Email"
+              id="email"
+              onChange={handleInputChange}
+              value={formData.email}
+            />
+            <Input
+              name="password"
+              placeholder="Password"
+              id="password"
+              onChange={handleInputChange}
+              value={formData.password}
+            />
+            <button type="submit" className="btn">
+              Register
+            </button>
+          </form>
         </div>
       </div>
     </Layout>
