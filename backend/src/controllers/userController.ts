2bd7229a116cf29f0fc45f314fc92dc48c34eb7e
Welcome ! <usmanubaid9@gmail.com>
2024-01-17 18:00:12 +0100

    fix: send correct status codes and msgs

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -12,53 +12,53 @@ const userController = {
 
     try {
       if (!username || !email || !password) {
-        return res.status(400).json({ message: "Please fill all the fields" });
+        return res.status(400).json({ error: "Please fill all the fields" });
       }
 
       const dbEmail = await checkExistingEmail(email);
       if (dbEmail.length > 0) {
-        return res
-          .status(400)
-          .json({ message: "This email is already in use" });
+        return res.status(400).json({ error: "This email is already in use" });
       }
 
       const dbUsername = await checkExistingUsername(username);
       if (dbUsername.length > 0) {
-        return res.status(400).json({ message: "This username is taken" });
+        return res.status(400).json({ error: "This username is taken" });
       }
 
       const user = await insertUserDb(username, email, password);
 
-      return res.json({
+      return res.status(200).json({
         message: "success",
         data: { id: user.insertId, token: generateJWT(user.insertId) },
       });
     } catch (error) {
       console.log("Error registering user", error);
-      return res.status(500).json({ message: "Internal server error" });
+      return res.status(500).json({ error: "Server error" });
     }
   },
 
   loginUser: async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
-    const user = await checkExistingEmail(email);
+    try {
+      const user = await checkExistingEmail(email);
 
-    if (user.length > 0) {
-      const { id, username, email, password: dbpassword } = user[0];
-      const checkPassword = await comparePassword(password, dbpassword);
-      if (checkPassword) {
-        return res
-          .status(200)
-          .json({
+      if (user.length > 0) {
+        const { id, username, email, password: dbpassword } = user[0];
+        const checkPassword = await comparePassword(password, dbpassword);
+        if (checkPassword) {
+          return res.status(200).json({
             message: "Success",
             data: { id, username, email, token: generateJWT(id) },
           });
+        }
+      } else {
+        return res.status(404).json({ error: "Email not found" });
       }
-    } else {
-      return res.status(400).json({ message: "Email not found" });
+      return res.status(401).json({ error: "Invalid credentials" });
+    } catch (error) {
+      return res.status(500).json({ error: "Server Error" });
     }
-    return res.status(400).json({ message: "Invalid credentials" });
   },
 };
 
