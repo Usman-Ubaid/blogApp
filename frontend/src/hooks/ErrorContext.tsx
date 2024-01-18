874ac595ef5d69557e6a97d9b0caaa327edf99da
Welcome ! <usmanubaid9@gmail.com>
2024-01-18 15:53:04 +0100

    refactor: add success msg in context

--- frontend/src/hooks/ErrorContext.tsx
+++ frontend/src/hooks/ErrorContext.tsx
@@ -3,18 +3,25 @@ import { createContext, useContext, useState } from "react";
 type ErrorContextType = {
   errorMsg: string;
   setErrorMsg: React.Dispatch<React.SetStateAction<string>>;
+  successMsg: string;
+  setSuccessMsg: React.Dispatch<React.SetStateAction<string>>;
 };
 
 export const ErrorContext = createContext<ErrorContextType>({
   errorMsg: "",
   setErrorMsg: () => null,
+  successMsg: "",
+  setSuccessMsg: () => null,
 });
 
 export const ErrorProvider = ({ children }: { children: React.ReactNode }) => {
   const [errorMsg, setErrorMsg] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
 
   return (
-    <ErrorContext.Provider value={{ errorMsg, setErrorMsg }}>
+    <ErrorContext.Provider
+      value={{ errorMsg, setErrorMsg, successMsg, setSuccessMsg }}
+    >
       {children}
     </ErrorContext.Provider>
   );
