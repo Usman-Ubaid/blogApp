48023c02abfc075daf3229a56eddd49c0a36d428
Welcome ! <usmanubaid9@gmail.com>
2024-01-18 15:53:35 +0100

    feat: use context hook

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,25 +1,37 @@
+import axios from "axios";
 import Layout from "../components/common/Layout";
 import Input from "../components/form/Input";
 import { useForm } from "../hooks/useForm";
 import { LoginFormData } from "../types/form";
 import { loginApi } from "../services/api/Auth";
-import axios from "axios";
 import { handleLoginError } from "../utils/handleAuthErrors";
+import { useError } from "../hooks/ErrorContext";
 
 const Login = () => {
   const { formData, handleInputChange } = useForm<LoginFormData>({
     email: "",
     password: "",
   });
+  const { errorMsg, setErrorMsg } = useError();
+  const { successMsg, setSuccessMsg } = useError();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      return await loginApi(formData);
+      return await loginApi(formData).then(() => {
+        setSuccessMsg("Successfully logged in");
+        setTimeout(() => {
+          setSuccessMsg("");
+        }, 3000);
+      });
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
         const err = handleLoginError(error.response?.status);
-        console.log(err);
+        setErrorMsg(err);
+
+        setTimeout(() => {
+          setErrorMsg("");
+        }, 3000);
       }
     }
   };
@@ -28,6 +40,9 @@ const Login = () => {
     <Layout>
       <div className="form-container">
         <div className="form-wrapper">
+          {errorMsg && <p className="error-msg">{errorMsg}</p>}
+          {successMsg && <p className="success-msg">{successMsg}</p>}
+
           <h2>Login</h2>
           <form onSubmit={handleSubmit}>
             <Input
