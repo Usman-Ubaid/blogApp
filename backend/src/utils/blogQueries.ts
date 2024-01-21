c6e50a77c3a34931c71346ac39e756700a39093f
Welcome ! <usmanubaid9@gmail.com>
2024-01-21 21:07:02 +0100

    feat: get all blogs query

--- backend/src/utils/blogQueries.ts
+++ backend/src/utils/blogQueries.ts
@@ -17,3 +17,20 @@ export const insertBlog: (
     );
   });
 };
+
+export const getAllBlogs: () => Promise<
+  {
+    id: number;
+    heading: string;
+    body: string;
+  }[]
+> = () => {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM blogs", (error, results) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(results);
+    });
+  });
+};
