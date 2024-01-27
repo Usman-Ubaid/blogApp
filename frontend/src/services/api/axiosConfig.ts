affc8c5bdfd95186bf197c7bb4585b7dff0d8f80
Welcome ! <usmanubaid9@gmail.com>
2024-01-27 22:58:36 +0100

    feat: intercepting requests

--- frontend/src/services/api/axiosConfig.ts
+++ frontend/src/services/api/axiosConfig.ts
@@ -1,8 +1,26 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3001/api";
+const BASE_URL = "http://localhost:3001/api";
 
 export const axiosInstance = axios.create({
-  baseURL: baseURL,
+  baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
 });
+
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+});
+
+axiosPrivate.interceptors.request.use(
+  (config) => {
+    const authToken = localStorage.getItem("auth-token");
+    if (authToken) {
+      config.headers.Authorization = `Bearer ${authToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
