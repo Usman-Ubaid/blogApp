cdb95de0d4833529c519101c88c3b9671d0993d0
Welcome ! <usmanubaid9@gmail.com>
2024-01-27 14:56:54 +0100

    feat: protect the routes

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import blogController from "../controllers/blog";
+import { protect } from "../middleware/authMiddleware";
 
 const router = Router();
 
-router.get("/blog", blogController.getAllBlogs);
-router.get("/blog/:id", blogController.getBlogById);
-router.post("/blog", blogController.createBlog);
-router.put("/blog/:id", blogController.editBlog);
-router.delete("/blog/:id", blogController.deleteBlog);
+router.get("/blog", protect, blogController.getAllBlogs);
+router.get("/blog/:id", protect, blogController.getBlogById);
+router.post("/blog", protect, blogController.createBlog);
+router.put("/blog/:id", protect, blogController.editBlog);
+router.delete("/blog/:id", protect, blogController.deleteBlog);
 
 export default router;
