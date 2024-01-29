a8b4c7f1598f0e6cf4bff944babdb1718c761e6c
Welcome ! <usmanubaid9@gmail.com>
2024-01-29 14:09:36 +0100

    feat: format date & style blog comp

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -4,11 +4,27 @@ import { useBlog } from "../hooks/SingleBlogContext";
 const Blog = () => {
   const { selectedBlog } = useBlog();
   const { heading, body, created_at } = selectedBlog;
+
+  const originalDate = new Date(created_at);
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const formattedDate = originalDate.toLocaleDateString("en-US", options);
+
   return (
     <Layout>
-      <h1>{heading}</h1>
-      <span>{created_at}</span>
-      <p>{body}</p>
+      <div className="blog-details">
+        <div className="blog-head">
+          <h1>{heading}</h1>
+          <span>{formattedDate}</span>
+          <span>Edit Post</span>
+        </div>
+        <div className="blog-content">
+          <p>{body}</p>
+        </div>
+      </div>
     </Layout>
   );
 };
