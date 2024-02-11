9005371afb8c8cd44fbb94bc9745af86ef3ea084
Welcome ! <usmanubaid9@gmail.com>
2024-02-11 20:59:51 +0100

    feat: use firebase for image storage

--- backend/src/types/env.d.ts
+++ backend/src/types/env.d.ts
@@ -9,6 +9,13 @@ declare global {
       DB_PASSWORD: string;
       DB_DATABASE: string;
       JWT_SECRET: string;
+      FIREBASE_API_KEY: string;
+      FIREBASE_AUTH_DOMAIN: string;
+      FIREBASE_PROJECT_ID: string;
+      FIREBASE_STORAGE_BUCKET: string;
+      FIREBASE_MESSAGING_SENDER_ID: string;
+      FIREBASE_APP_ID: string;
+      FIREBASE_MEASUREMENT_ID: string;
     }
   }
 }
