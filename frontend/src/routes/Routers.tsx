44a8fcefddb668477a22f176a4cb123aa813e1ea
Welcome ! <usmanubaid9@gmail.com>
2024-02-13 22:29:25 +0100

    refactor: lazy load components

--- frontend/src/routes/Routers.tsx
+++ frontend/src/routes/Routers.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ProtectedRoutes from "../privateRoutes/ProtectedRoutes";
-import WriteBlog from "../pages/WriteBlog";
-import Dashboard from "../pages/Dashboard";
-import BlogsList from "../pages/BlogsList";
-import Blog from "../pages/Blog";
-import UpdateBlog from "../pages/UpdateBlog";
+
+const WriteBlog = lazy(() => import("../pages/WriteBlog"));
+const Dashboard = lazy(() => import("../pages/Dashboard"));
+const BlogsList = lazy(() => import("../pages/BlogsList"));
+const Blog = lazy(() => import("../pages/Blog"));
+const UpdateBlog = lazy(() => import("../pages/UpdateBlog"));
 
 const Routers = () => {
   const routes = [
@@ -24,11 +26,46 @@ const Routers = () => {
   return (
     <Routes>
       <Route element={<ProtectedRoutes />}>
-        <Route path="/writeBlog" element={<WriteBlog />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/blogs" element={<BlogsList />} />
-        <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/updateBlog/:id" element={<UpdateBlog />} />
+        <Route
+          path="/writeBlog"
+          element={
+            <Suspense>
+              <WriteBlog />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <Suspense>
+              <Dashboard />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/blogs"
+          element={
+            <Suspense>
+              <BlogsList />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/blog/:id"
+          element={
+            <Suspense>
+              <Blog />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/updateBlog/:id"
+          element={
+            <Suspense>
+              <UpdateBlog />
+            </Suspense>
+          }
+        />
       </Route>
       {routes.map((route) => (
         <Route key={route.id} path={route.path} element={route.element} />
