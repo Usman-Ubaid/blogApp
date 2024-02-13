23f3a506c1e6d5cd2bd83a992f23d32f5ce37f1e
Welcome ! <usmanubaid9@gmail.com>
2024-02-13 23:49:33 +0100

    refactor code and style

--- frontend/src/components/blog/BlogCards.tsx
+++ frontend/src/components/blog/BlogCards.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { BlogCardProps } from "../../types/blog";
 import { useAuth } from "../../hooks/AuthContext";
-import { formatDate } from "../../utils/formatDate";
 
 const BlogCard = ({ data }: BlogCardProps) => {
   const navigate = useNavigate();
@@ -11,13 +10,14 @@ const BlogCard = ({ data }: BlogCardProps) => {
     <div className="blog-card-wrapper">
       {data &&
         data.map((item) => (
-          <div className="blog-card" key={item.id}>
-            <span>
-              {currentUser.username} - {formatDate(item.created_at)}
-            </span>
-            <h2 onClick={() => navigate(`/blog/${item.id}`)}>
-              {item?.heading}
-            </h2>
+          <div
+            role="link"
+            className="blog-card"
+            key={item.id}
+            onClick={() => navigate(`/blog/${item.id}`)}
+          >
+            <span>{currentUser.username}</span>
+            <h2>{item?.heading}</h2>
           </div>
         ))}
     </div>
